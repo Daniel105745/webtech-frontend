1023925447b720fc1f58e8b793bcdb3b95d96224
Daniel <liss.daniel@gmx.de>
2025-11-07 10:43:33 +0100

    Right Ordinary

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import PlansView from '../views/PlansView.vue'
-import WorkoutsView from '../views/WorkoutsView.vue'
-import ExercisesView from '../views/ExercisesView.vue'
+
 
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/plans', component: PlansView },
-  { path: '/workouts', component: WorkoutsView },
-  { path: '/exercises', component: ExercisesView }
+
 ]
 
 
