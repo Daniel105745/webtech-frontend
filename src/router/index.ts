7c4f9b1eb39616c9e18b6644e9a532ed7794e095
Daniel <liss.daniel@gmx.de>
2025-11-08 21:45:55 +0100

    Test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from "../views/HomeView.vue";
-import PlansView from "../views/PlansView.vue";
+import HomeView from "../view/HomeView.vue";
+import PlansView from "../view/PlansView.vue";
+import ExerciseView from "../view/ExerciseView.vue";
+import WorkoutsView from "../view/WorkoutsView.vue";
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/', component: PlansView }
+  { path: '/plans', component: PlansView },
+  {path: '/exercise', component: ExerciseView},
+  {path: '/workouts', component: WorkoutsView}
 
 ]
 export default createRouter({
